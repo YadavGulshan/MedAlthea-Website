4a3e775630b40c6a9268954cd7155c999c43d5a3
rahul007-bit <yadavrahul8141@yahoo.com>
2022-03-01 17:12:15 +0530

    make nav resonsive and intropage for md devices

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useState } from 'react'
 import styles from '../../styles/header.module.css'
 const HeaderComponent = () => {
+  const [isVisible, setVisible] = useState(false)
+  const block = isVisible ? '' : 'hidden'
   const primaryMenuItem = [
     { item: 'About', href: '#' },
     { item: 'Services', href: '#' },
@@ -14,14 +16,12 @@ const HeaderComponent = () => {
           <div className="flex justify-between">
             <div>
               <a href="#" className="flex items-center py-4 px-2">
-                <span
-                  className={`  font-Russo text-3xl font-[550] text-black `}
-                >
+                <span className={`font-Russo text-3xl font-[550] text-black `}>
                   MedAlthea
                 </span>
               </a>
             </div>
-            <div className="items-center space-x-1 md:flex">
+            <div className="hidden items-center space-x-1 md:flex ">
               {primaryMenuItem.map((item, index) => (
                 <a
                   key={index}
@@ -46,7 +46,42 @@ const HeaderComponent = () => {
                 Register
               </a>
             </div>
+            <div className="flex items-center md:hidden">
+              <button
+                onClick={() => setVisible(!isVisible)}
+                className="outline-none"
+              >
+                <svg
+                  className="h-8 w-8 text-[#1D2B4F]"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path d="M4 6h16M4 12h16M4 18h16"></path>
+                </svg>
+              </button>
+            </div>
           </div>
+        </div>
+
+        <div
+          className={`my-2 mx-auto max-w-6xl px-4 transition-[display] duration-300 md:hidden ${block}`}
+        >
+          <ul className="">
+            {primaryMenuItem.map((items, index) => (
+              <li key={index}>
+                <a
+                  href="#services"
+                  className="block px-2 py-4 text-sm transition duration-300 hover:bg-green-500"
+                >
+                  {items.item}
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
       </nav>
     </>
--- components/intropage/intropage.tsx
+++ components/intropage/intropage.tsx
@@ -3,9 +3,9 @@ import style from '../../styles/intropage.module.css'
 const IntroPage = () => {
   return (
     <>
-      <section className="my-2 mx-auto h-fit max-w-6xl bg-white">
-        <div className="flex h-full justify-between">
-          <div className="mx-2 my-32 h-full w-2/3">
+      <section className="my-2 mx-4 h-fit bg-white xl:mx-auto xl:max-w-6xl">
+        <div className="flex h-full flex-col items-center justify-between xl:flex-row">
+          <div className="mx-2 my-32 h-full xl:w-2/3">
             <h1 className="py-5 font-Inter text-6xl font-extrabold text-[#1D2B4F]">
               We are providing you the access to all your health care needs.
             </h1>
@@ -20,7 +20,7 @@ const IntroPage = () => {
             />
           </div>
 
-          <div className="flex w-1/3 items-center text-center ">
+          <div className="flex items-center xl:w-1/3 ">
             {/* <div className={`${style.shopImage} rounded-full`}></div> */}
             <img
               className="mb-10 h-96 w-96 rounded-full"
