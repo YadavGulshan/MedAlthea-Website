e24552872e369d78a14af81104c88440a2a45aae
rahul007-bit <yadavrahul8141@yahoo.com>
2022-03-08 21:56:31 +0530

    small changes on mobile view

--- components/Header/widget/mobilemenu.tsx
+++ components/Header/widget/mobilemenu.tsx
@@ -4,10 +4,7 @@ import Drawer from '@mui/material/Drawer'
 import Button from '@mui/material/Button'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
-import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
-import InboxIcon from '@mui/icons-material/MoveToInbox'
-import MailIcon from '@mui/icons-material/Mail'
 
 type Anchor = 'menu'
 
