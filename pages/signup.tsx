2b830e2651b0a0d99c405070396c18995ef5a1bc
rahul007-bit <yadavrahul8141@yahoo.com>
2022-03-12 18:14:42 +0530

    make font bold at sigup page

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -3,7 +3,7 @@ import Main from '../layout/Main'
 const SignUp = () => {
   return (
     <Main>
-      <section className="h-[88vh]">
+      <section className="h-[88vh] select-none">
         <div className="my-2 mx-auto flex h-full w-full max-w-6xl flex-col items-center justify-center px-4 md:w-3/4">
           <div className="mx-10 w-full sm:w-[448px]">
             <h1 className=" mb-10 min-w-full text-center font-Inter text-5xl font-bold text-[#1D2B4F]">
@@ -28,16 +28,16 @@ const SignUp = () => {
               </div>
               <div>
                 <input
-                  className="my-4 h-14 w-full rounded-full border-2 border-none bg-blue-500 px-6 text-white outline-none transition duration-300 hover:border-blue-300 hover:shadow-xl hover:shadow-blue-400 md:px-10"
+                  className="my-4 h-14 w-full rounded-full border-2 border-none bg-blue-500 px-6 font-bold text-white outline-none transition duration-300 hover:border-blue-300 hover:shadow-xl hover:shadow-blue-400 md:px-10"
                   type="submit"
-                  placeholder="Log in"
+                  value="Log in"
                 />
               </div>
-              <div className="my-5 flex flex-col items-center justify-evenly sm:flex-row">
+              <div className="my-5 flex flex-col items-center justify-evenly font-Inter font-semibold sm:flex-row">
                 <p>Not have an account yet?</p>
                 <Link
                   underline="none"
-                  className="my-2 rounded-md bg-blue-500 py-2 px-4 text-white shadow-xl shadow-blue-400"
+                  className="my-2 rounded-md bg-blue-500 py-2 px-4 font-bold text-white shadow-xl shadow-blue-400"
                   href="/register"
                 >
                   Register now
