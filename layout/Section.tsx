cf8298a736fd4589c8bbd86745782b4ade2cc9a6
rahul007-bit <yadavrahul8141@yahoo.com>
2022-03-13 13:41:36 +0530

    working on regiter page

--- layout/Section.tsx
+++ layout/Section.tsx
@@ -1,8 +1,8 @@
 import React, { FC } from 'react'
 const Section: FC = ({ children }) => {
   return (
-    <section className="my-2  mx-auto h-fit bg-white xl:max-w-6xl">
-      <div className="max-w-6xlpx-4 h-full w-full md:w-3/4">{children}</div>
+    <section className="my-2 mx-auto h-[88vh] bg-white xl:max-w-6xl">
+      {children}
     </section>
   )
 }
