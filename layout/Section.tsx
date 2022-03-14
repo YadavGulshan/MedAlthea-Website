f975e7caeb70f08dfe7d7253d12f16dc2ae748ea
rahul007-bit <yadavrahul8141@yahoo.com>
2022-03-15 00:02:39 +0530

    done with register page at alpha branch

--- layout/Section.tsx
+++ layout/Section.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react'
 const Section: FC = ({ children }) => {
   return (
-    <section className="my-2 mx-auto h-[88vh] bg-white xl:max-w-6xl">
+    <section className="my-10 mx-auto bg-white md:h-[88vh] xl:max-w-6xl">
       {children}
     </section>
   )
