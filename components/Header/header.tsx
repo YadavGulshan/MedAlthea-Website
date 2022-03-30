c10cb4a3bf4e1eff6666e5130be3a2d4968e6b0b
ChiragPadyal <freakstar03@gmail.com>
2022-03-30 23:58:06 +0530

    Added Night and Day mode to Header and working on dashboard so added to gitignore

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -1,3 +1,8 @@
+// darkmode
+// dark:bg-gray-800
+// text-gray-500
+// dark:bg-gray-500 dark:text-white for Registration Btn
+
 import React from 'react';
 import Box from '@mui/material/Box';
 import Link from 'next/link';
@@ -10,12 +15,12 @@ function HeaderComponent() {
     { item: 'Contact', href: '/contact' },
   ];
   return (
-    <nav className="bg-white ">
-      <div className=" my-2 mx-auto max-w-6xl px-4">
+    <nav className="bg-white dark:bg-gray-800 ">
+      <div className=" mx-auto mb-2 max-w-6xl px-4">
         <div className="flex justify-between">
           <div>
             <div className="flex items-center py-4 px-2">
-              <div className="font-Russo text-3xl font-[550] text-black ">
+              <div className="font-Russo text-3xl font-[550] text-black dark:text-gray-500 ">
                 <Link href="/">MedAlthea</Link>
               </div>
             </div>
@@ -26,10 +31,10 @@ function HeaderComponent() {
             )}
           </div>
           <div className="hidden items-center space-x-3 px-2 md:flex ">
-            <div className="rounded py-2 px-2 text-lg font-semibold text-black after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-[#0E63F3] after:transition after:duration-500 after:ease-out hover:after:origin-bottom-left hover:after:scale-x-100">
+            <div className="rounded py-2 px-2 text-lg font-semibold text-black after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-[#0E63F3] after:transition after:duration-500 after:ease-out hover:after:origin-bottom-left hover:after:scale-x-100 dark:text-gray-500">
               <Link href="/signup">Sign In</Link>
             </div>
-            <div className="rounded-lg bg-blue-500 py-2 px-4 text-xl font-semibold !text-white shadow-lg shadow-blue-500 hover:!text-green-500 hover:shadow-green-500 ">
+            <div className="rounded-lg bg-blue-500 py-2 px-4 text-xl font-semibold !text-white shadow-lg  shadow-blue-500 hover:!text-green-500  hover:shadow-green-500 dark:bg-gray-500 ">
               <Link href="/register">Register</Link>
             </div>
           </div>
@@ -47,7 +52,7 @@ function TabsLabel(lb: string, hr: string, index: number) {
   return (
     <Box sx={{ width: '100%' }} key={index}>
       <div className="py-4 px-5">
-        <p className="relative text-lg font-semibold text-[#1D2B4F] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-[#0E63F3] after:transition after:duration-500 after:ease-out hover:after:origin-bottom-left hover:after:scale-x-100">
+        <p className="relative text-lg font-semibold text-[#1D2B4F] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-[#0E63F3] after:transition after:duration-500 after:ease-out hover:after:origin-bottom-left hover:after:scale-x-100 dark:text-gray-500">
           <Link href={hr}>{lb}</Link>
         </p>
       </div>
